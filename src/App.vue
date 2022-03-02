15cd520ed6e1a59d950f6a965cf8d2444d15b104
Timofei <t.kruber@2dit.ru>
2022-03-02 11:04:59 +0600

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main">
+        <form>
+            <h4>Создание поста</h4>
+            <input class="" type="text" placeholder="Название поста">
+            <input type="text" placeholder="Название описания">
+            <button>Создаnm</button>
+        </form>
+        <div class="post" v-for="post in posts" :key="post.posts">
+            <div> <strong> Название:</strong> {{post.title}} </div>
+            <div> <strong> Описание:</strong> {{post.body}} </div>
+        </div>
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            posts:[
+                {id:1, title: 'Javascript', body: 'Описание поста'},
+                {id:2, title: 'Javascript 2', body: 'Описание поста 2'},
+                {id:3, title: 'Javascript 3', body: 'Описание поста 3'},
+            ]
+        }
+    },
+    methods: {
+        addLike(){
+            this.likes += 1;
+        },
+        disLike(){
+            this.dislikes += 1;
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.post{
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
 }
 </style>
